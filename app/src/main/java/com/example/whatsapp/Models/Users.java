362b82dda639f7e0e8d9f01b192fa2b242557d9a
SuryankDixit <suryankdixit@gmail.com>
2022-01-20 08:54:14 +0530

    Firebase sign up with email and password

---
+++ app/src/main/java/com/example/whatsapp/Models/Users.java
@@ -0,0 +1,4 @@
+package com.example.whatsapp.Models;
+
+public class Users {
+}
--- app/src/main/java/com/example/whatsapp/SignUpActivity.java
+++ app/src/main/java/com/example/whatsapp/SignUpActivity.java
@@ -3,15 +3,35 @@ package com.example.whatsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.whatsapp.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // hiding toolbar
         getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.createUserWithEmailAndPassword(binding.etEmail.getText().toString(),binding.etPassword.getText().toString());
+            }
+        });
     }
 }
